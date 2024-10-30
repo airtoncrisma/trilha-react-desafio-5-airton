f6d7959ab8ffcd69158593e12416a1e8e0ac90d2
airtoncrisma <atopomap@outlook.com>
2024-10-30 19:14:35 -0300

    update

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://ymaubtndgxvntvirvrvx.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltYXVidG5kZ3h2bnR2aXJ2cnZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAzMjQ5MDMsImV4cCI6MjA0NTkwMDkwM30.uOIM19ulHFdsQmvO2cwtBYzFCPZRjfSQF6qrhMJeB6I",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltYXVidG5kZ3h2bnR2aXJ2cnZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAzMjQ5MDMsImV4cCI6MjA0NTkwMDkwM30.uOIM19ulHFdsQmvO2cwtBYzFCPZRjfSQF6qrhMJeB6I"
     }
 })
